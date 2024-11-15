7203d83cbf639ae1b298dac70977c7fe49bce9f6
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2024-11-15 18:07:42 +0000

    ci: copy over files from other repos (rendered)

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,5 +6,13 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('browserconfig.xml');
   eleventyConfig.addPassthroughCopy('site.webmanifest');
-  eleventyConfig.addPassthroughCopy('stylesheets');
+  eleventyConfig.addPassthroughCopy({ '_tmp/bak/blog': 'blog' });
+  eleventyConfig.addPassthroughCopy({
+    '_tmp/bak/intro-to-web-dev-course': 'intro-to-web-dev-course',
+  });
+  eleventyConfig.addPassthroughCopy({ '_tmp/bak/blog': 'blog' });
+  eleventyConfig.addPassthroughCopy({
+    '_tmp/bak/trivia-trundle': 'trivia-trundle',
+  });
+  eleventyConfig.addPassthroughCopy({ '_tmp/bak/weeks-to-go': 'weeks-to-go' });
 }
