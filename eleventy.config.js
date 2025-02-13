a0ab0aa2f3cbdf58043303f580672ba3e2460c25
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-02-13 15:31:03 +0000

    refactor: use 11ty image plugin and partials, update IT consultant photo

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('stylesheets');
   eleventyConfig.addPassthroughCopy('mstile-*.png');
   eleventyConfig.addPassthroughCopy('favicon*');
   eleventyConfig.addPassthroughCopy('android-chrome*');
   eleventyConfig.addPassthroughCopy('apple-touch-icon*');
-  eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('browserconfig.xml');
   eleventyConfig.addPassthroughCopy('site.webmanifest');
   eleventyConfig.addPassthroughCopy({ '_tmp/bak/blog': 'blog' });
@@ -15,4 +17,17 @@ export default async function (eleventyConfig) {
     '_tmp/bak/trivia-trundle': 'trivia-trundle',
   });
   eleventyConfig.addPassthroughCopy({ '_tmp/bak/weeks-to-go': 'weeks-to-go' });
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ['avif', 'webp', 'svg'],
+    svgShortCircuit: true,
+    outputDir: "/img/",
+    failOnError: true,
+    transformOnRequest: false,
+    widths: ["auto", 400, 800, 1200],
+    htmlOptions: {
+			imgAttributes: {
+				sizes: "(min-width: 1024em) 400px, 100vw"
+			},
+		},
+  });
 }
