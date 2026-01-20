7ffa60b436a972e71493582a8e225cd084a4eeb6
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2026-01-20 18:45:15 +0000

    refactor: migrate blog to 11ty (using Eleventy Base Blog v9)

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+export default {
+	title: 'LouieChristie.com',
+	url: 'https://www.louiechristie.com/',
+	language: 'en',
+	description:
+		'Louie Christie - Adventurous | Tech Geek | Underground Comedian in my own head 😬 -> Homepage',
+	author: {
+		name: 'Louie Christie',
+		email: 'hello@louiechristie.com',
+		url: 'https://www.louiechristie.com/about/',
+	},
+};
